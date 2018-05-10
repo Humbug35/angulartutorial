5363c0a9c1fc3e980ce78ea348fb1adb83e8e714
Linus Plume <plumee_91@hotmail.com>
2018-05-10 09:09:29 +0200

    angulartutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice'},
+  { id: 12, name: 'Mr. FeelGood'},
+  { id: 13, name: 'Mr. Bad'},
+  { id: 14, name: 'Mr. Doctor'},
+  { id: 15, name: 'Mr. Cool'},
+  { id: 16, name: 'Mr. Fronda'},
+  { id: 17, name: 'Mr. TruckKeps'},
+  { id: 18, name: 'Mr. Mouth'},
+  { id: 19, name: 'Mr. DandV'},
+  { id: 20, name: 'Mr. Sultan'}
+];
